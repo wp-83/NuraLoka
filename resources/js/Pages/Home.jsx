b3dca719cf7f6414f25ba24f899e1f199bd35f75
agnesgonxha <gonxhaagnes12@gmail.com>
2026-04-09 16:12:12 +0700

    update: coba1

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -3,6 +3,7 @@ export default function Home({ title }){
         <>
             <p>Hai, ini React!</p>
             <p>Test commit and push</p>
+            <p>Aku Anesssssssssssssssss!</p>
         </>
     );
 }
