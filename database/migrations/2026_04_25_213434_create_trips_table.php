ac1596839dc830806475308b1de7791b5d351eda
Jeys <jayadisteven1@gmail.com>
2026-04-25 22:13:29 +0700

    add: some migrations

---
+++ database/migrations/2026_04_25_213434_create_trips_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('title');
+            $table->string('origin_name');
+            $table->decimal('origin_latitude',10,8);
+            $table->decimal('origin_longitude', 11,8);
+            $table->string('destination_name');
+            $table->decimal('destination_latitude',10,8);
+            $table->decimal('destination_longitude', 11,8);
+            $table->date('trip_date');
+            $table->boolean('is_public');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trips');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
 use App\Http\Controllers\MapController;
-use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
     return inertia('Home', [
