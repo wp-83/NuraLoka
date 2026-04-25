ddbcd2baa26f1a6ed076192429891d22913e07b5
agnesgonxha <gonxhaagnes12@gmail.com>
2026-04-25 21:35:45 +0700

    add: linter test

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\MapController;
 use Inertia\Inertia;
+use App\Http\Controllers\MapController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
