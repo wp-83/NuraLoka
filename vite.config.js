06022f6e1ad89d4f6d1da2897887483cee56de46
William P. <williampratama837@gmail.com>
2026-04-30 16:40:12 +0700

    add: shortcut for accessing css and js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
@@ -12,4 +13,11 @@ export default defineConfig({
         tailwindcss(),
         react(),
     ],
+
+    resolve: {
+        alias: {
+            '@js': path.resolve(__dirname, 'resources/js'),
+            '@css': path.resolve(__dirname, 'resources/css'),
+        },
+    },
 });
