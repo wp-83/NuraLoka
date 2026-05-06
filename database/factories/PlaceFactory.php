0689f492a8df28c7771dbdc3d54cdeca99a1661d
agnesgonxha <gonxhaagnes12@gmail.com>
2026-05-06 16:04:42 +0700

    Membuat example factory dan seeder data destinasi

--- database/factories/PlaceFactory.php
+++ database/factories/PlaceFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\Place;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -20,12 +21,12 @@ class PlaceFactory extends Factory
         $name = $this->faker->unique()->words(rand(2, 4), true);
         $name = ucwords($name);
 
-        // Koordinat sekitar Indonesia
+        // Coordinates around Indonesia
         return [
             'name' => $name,
             'slug' => Str::slug($name),
             'description' => $this->faker->paragraph(3),
-            'latitude' => $this->faker->latitude(-8.5, -6.0),   // Pulau Jawa
+            'latitude' => $this->faker->latitude(-8.5, -6.0),   // Java Island
             'longitude' => $this->faker->longitude(106.0, 111.0),
             'address' => $this->faker->address(),
         ];
