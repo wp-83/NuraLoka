3d91eea558dca32823014069646b54adcc83052e
William P. <williampratama837@gmail.com>
2026-05-07 08:47:54 +0700

    fix: constrained foreign key type

--- database/migrations/2026_04_25_213151_create_user_details_table.php
+++ database/migrations/2026_04_25_213151_create_user_details_table.php
@@ -13,13 +13,13 @@ return new class extends Migration
     {
         Schema::create('user_details', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id');
-            $table->integer('province_id');
-            $table->string('name');
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('province_id')->constrained();
+            $table->string('fullname');
             $table->string('profile_path')->nullable();
             $table->date('dob');
             $table->enum('gender', ['male', 'female', 'unspecified']);
-            $table->integer('total_points');
+            $table->integer('total_points')->default(0);
             $table->timestamps();
         });
     }
