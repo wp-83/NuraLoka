65b05008813b42c6161fd84665e2c7911d1a8651
Xullfikar <andizulfikar831@gmail.com>
2026-05-08 10:50:22 +0700

    update: automation slug creation

--- app/Models/Place.php
+++ app/Models/Place.php
@@ -4,10 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Place extends Model
 {
     use HasFactory;
+    use HasSlug;
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
 
     protected $fillable = [
         'name', 'slug', 'description',
